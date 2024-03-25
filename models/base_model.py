2d71b31bf9bbfcab6572b242e0df9387f412ae2b
Sherif Awad <sherif.hero1@gmail.com>
2024-03-25 16:55:36 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -46,27 +46,25 @@ class BaseModel:
             *args: Unused arguments (for future compatibility).
             **kwargs: Keyword arguments used to set object attributes.
         """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
-                else:
-                    setattr(self, key, value)
-
-        models.storage.new(self)
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def save(self):
         """
         Updates the updated_at timestamp and saves the object to storage.
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
@@ -74,7 +72,7 @@ class BaseModel:
         Returns a dictionary representation of the object with
         formatted timestamps.
         """
-        inst_dict = self.__dict__.copy()
+        inst_dict = dict(self.__dict__)
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
