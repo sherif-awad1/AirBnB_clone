f26ad57516e15f6116ff8f683b5e811e1f59eb05
Sherif Awad <sherif.hero1@gmail.com>
2024-03-25 17:04:12 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -33,56 +33,45 @@ class BaseModel:
             formatted timestamps.
         __str__(self):
             Returns a string representation of
-            the object including its class name,
-            ID, and all attributes.
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the object with a unique ID, timestamps, and
-        optional keyword arguments.
+            """
+        def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
 
         Args:
-            *args: Unused arguments (for future compatibility).
-            **kwargs: Keyword arguments used to set object attributes.
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        if (len(kwargs) == 0):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
+            models.storage.new(self)
 
     def save(self):
-        """
-        Updates the updated_at timestamp and saves the object to storage.
-        """
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary representation of the object with
-        formatted timestamps.
-        """
-        inst_dict = dict(self.__dict__)
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
+        """Return the dictionary of the BaseModel instance.
 
-        return inst_dict
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
 
     def __str__(self):
-        """
-        Returns a string representation of the object including its class name,
-        ID, and all attributes.
-        """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
