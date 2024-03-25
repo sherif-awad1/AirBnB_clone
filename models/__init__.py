c61f3ee194dccab74a2f9b710183017a54553edd
Sherif Awad <sherif.hero1@gmail.com>
2024-03-25 22:40:35 +0200

    save

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
 """
 Import the FileStorage class from the file_storage module.
 """
-from models.engine.file_storage import FileStorage
+import models.engine.file_storage as file_storage
 """
 Instantiate a FileStorage object to manage object storage.
 """
-storage = FileStorage()
+storage = file_storage.FileStorage()
 """
 Load any existing objects from the JSON file into the storage dictionary.
 If the file doesn't exist, no action is taken.
